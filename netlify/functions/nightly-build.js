9f690535c6c3c97ccc9f999630ee4bf83da89080
Ben Smith <jsmith97@sky.com>
2025-07-26 23:11:24 +0100

    remove function

--- netlify/functions/nightly-build.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async () => {
-    const res = await fetch('https://api.netlify.com/build_hooks/68854ce5702b504f60421f87', {
-        method: 'POST'
-    });
-    return {
-        statusCode: 200,
-        body: 'Triggered build'
-    };
-};
